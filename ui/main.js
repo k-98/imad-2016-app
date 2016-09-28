6986281cae144f7491ecbaab0e4c666c1637cbd4
k-98 <tiwari.kishan12@gmail.com>
2016-09-27 17:12:19 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,9 @@
 console.log('Loaded!');
-
+//for changing the text
 var element =document.getElementById('main-text');
 element.innerHTML='Kishan tiwari';
+//Move the picture in app
+var img = document.getElementById('madi');
+img.onclick=function(){
+    img.style.marginLeft='100px';
+};
