7d763077b432eed12da81e52385a7af6620d3e9d
k-98 <tiwari.kishan12@gmail.com>
2016-09-29 07:11:27 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,14 @@
-console.log('Loaded!');
-//for changing the text
-var element =document.getElementById('main-text');
-element.innerHTML='Kishan tiwari';
-//Move the picture in app
-var img = document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+10;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-    var interval = setInterval(moveRight, 100);
-   // img.style.marginLeft='100px';
+// counter code
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function() {
+    // Make request to the counter end point
+    
+    //Capture the response and strore it in a variable
+    
+    //renderthe variable in the correct span
+    counter =counter+1;
+    var span = document.getElementById('count');
+    span.innerHTML= counter.toString();
+    
 };
