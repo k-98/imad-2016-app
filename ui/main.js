c03a4d110f985632d144cb8c35640653452223f1
k-98 <tiwari.kishan12@gmail.com>
2016-10-10 16:05:56 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,14 +26,24 @@ var nameInput = document.getElementById('name');
 var name=nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    //make the request to the server and send the name to
-    //capture a name and render it  as list
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for(var i=0; names.length; i++){
-        list+='<li>'+names[i]+'</li>';
+    if(request.readyState===XMLHttpRequest.DONE){
+        //takesome action
+        if(request.status===200){
+               var names = request.responseText;
+               names = JSON.parse(names);
+               var list = '';
+             for(var i=0; names.length; i++){
+             list+='<li>'+names[i]+'</li>';
         
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
+            
+        }
+    }
+    //make the request to the server and send the name to
+     request.open('GET', 'http://k-98.imad.hasura-app.io/submit-name?name='+name,true);
+     request.send(null);
+    //capture a name and render it  as list
+ 
 };
